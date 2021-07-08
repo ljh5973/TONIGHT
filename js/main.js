14e0f91b8f801557e7a3b2c47c5b1120f530b76a
AHNHYUNGKI <saoly2@naver.com>
2021-07-08 14:59:39 +0900

    첫번째 커밋

---
+++ js/main.js
@@ -0,0 +1,26 @@
+
+
+    
+window.onload=function(){
+
+    $('.hamBtn').click(function(ev){
+        ev.preventDefault();
+        $(this).toggleClass('active');
+        $('.header-user').toggleClass('active');
+    });
+
+    $('.navbar-item-down').click(function(ev){
+        ev.preventDefault();
+        $('.header-button').toggleClass('active');
+    });
+
+    $(".search-cancel").click(function(ev){
+        ev.preventDefault();
+        $('.header-search').toggleClass('active');
+    })
+
+    $(".navbar-item-search").click(function(ev){
+        ev.preventDefault();
+        $('.header-search').toggleClass('active');
+    })
+};
